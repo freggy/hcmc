98d9fb16674dd717d0f508a2c47457449d094e90
Yannic Rieger <yannic.rieger@hetzner-cloud.de>
2020-07-07 20:15:27 +0200

    Start work on fetching actions

--- src/main/kotlin/dev/freggy/hcmc/HcmcPlugin.kt
+++ src/main/kotlin/dev/freggy/hcmc/HcmcPlugin.kt
@@ -1,17 +1,33 @@
 package dev.freggy.hcmc
 
 import dev.freggy.hcmc.hcloud.HetznerCloud
+import dev.freggy.hcmc.hcloud.model.Server
+import kotlinx.coroutines.channels.Channel
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.concurrent.ConcurrentHashMap
 
 class HcmcPlugin : JavaPlugin() {
+
+    // pool of current servers mapped id -> server
+    val serverPool = ConcurrentHashMap<Int, Server>()
+    val serverEvents = Channel<Unit>()
 
     private var hcloud: HetznerCloud = TODO()
 
     override fun onEnable() {
         hcloud = HetznerCloud("")
+
+        Bukkit.getScheduler().runTaskTimerAsynchronously(this, Runnable {
+            // TODO:
+            //   * fetch actions with /actions?sort=finished:desc
+            //   * save last fetch timestamp to determine which actions are new
+            //   * distribute to corresponding channel
+        }, 0, 20)
     }
 
     override fun onDisable() {
-        hcloud.client.close()
+        this.serverEvents.close()
+        this.hcloud.client.close()
     }
 }
